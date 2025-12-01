32a61de4ca72fed10f95742aca7ba35d55f70389
Eugen Lang <sayanseliv@gmail.com>
2025-12-01 11:42:06 +0200

    install nuxt ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: '2025-07-15',
-	devtools: { enabled: true },
-	modules: ['@nuxt/eslint'],
-	typescript: {
-		strict: true,
-	},
+  css: ['/assets/css/main.css'],
+  modules: ['@nuxt/eslint', '@nuxt/ui'],
+  typescript: {
+    strict: true,
+  },
+  devtools: { enabled: true },
+  compatibilityDate: '2025-07-15',
 });
