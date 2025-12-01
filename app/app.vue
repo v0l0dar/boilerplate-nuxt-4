0e7e3d37521bf5e930c96e72d533c37ffc3623c1
Eugen Lang <sayanseliv@gmail.com>
2025-12-01 14:39:40 +0200

    update app.vue example

--- app/app.vue
+++ app/app.vue
@@ -1,24 +1,45 @@
 <template>
-  <main class="min-h-screen bg-TXT">
-    <div
-      class="h-32 rounded-lg bg-green text-white flex items-center justify-center">
-      green (DEFAULT = #01C11F)
+  <main class="min-h-screen space-y-8 p-8 font-mono text-sm">
+    <div class="grid grid-cols-4 gap-6">
+      <div
+        class="h-32 rounded-lg bg-TXT text-white flex items-center justify-center">
+        TXT (#0C0612)
+      </div>
+      <div
+        class="h-32 rounded-lg bg-BG text-white flex items-center justify-center">
+        BG (DEFAULT)
+      </div>
+      <div
+        class="h-32 rounded-lg bg-green text-white flex items-center justify-center">
+        green (DEFAULT = #01C11F)
+      </div>
+      <div
+        class="h-32 rounded-lg bg-green-light text-black flex items-center justify-center">
+        green-light
+      </div>
+      <div
+        class="h-32 rounded-lg bg-BG-quest text-black flex items-center justify-center">
+        BG-quest (#E9DCAE)
+      </div>
+      <div
+        class="h-32 rounded-lg bg-BG-hover text-black flex items-center justify-center">
+        BG-hover (с альфой)
+      </div>
+      <div
+        class="h-32 rounded-lg bg-BG-active text-black flex items-center justify-center">
+        BG-active (#F6D515)
+      </div>
     </div>
-    <div
-      class="h-32 rounded-lg bg-green-light text-black flex items-center justify-center">
-      green-light
-    </div>
-    <div
-      class="h-32 rounded-lg bg-BG-quest text-black flex items-center justify-center">
-      BG-quest (#E9DCAE)
-    </div>
-    <div
-      class="h-32 rounded-lg bg-BG-hover text-white flex items-center justify-center">
-      BG-hover (с альфой)
-    </div>
-    <div
-      class="h-32 rounded-lg bg-BG-active text-black flex items-center justify-center">
-      BG-active (#F6D515)
+    <div>
+      <h1>Your Application</h1>
+      <UButton
+        active
+        color="neutral"
+        variant="outline"
+        active-color="primary"
+        active-variant="solid">
+        Button
+      </UButton>
     </div>
   </main>
 </template>
